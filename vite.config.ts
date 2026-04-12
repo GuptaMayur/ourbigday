f372ee55720fcb7fd6771e10d5bf3086a0a72752
Mayur Gupta <mayurx@Mayurs-MacBook-Air.local>
2026-04-12 07:48:50 +0530

    fix: base path for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base: "/ourbigday/",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
